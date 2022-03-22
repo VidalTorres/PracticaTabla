9f16f0257c16bcfd32ae2fb33f349694e85d0261
Vidal Torres <vidaltorres2001@outlook.com>
2022-03-21 22:56:43 -0700

    Se agrega el proyecto

---
+++ src/Datos.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react"
+import './index.css'
+
+export const Datos = () => {
+  const [producto, setProducto] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch("http://scratchya.com.ar/react/datos.php")
+    const data = await response.json()
+    setProducto(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  return (
+      <table className="tabla">
+          <thead>
+              <tr>
+              <td>Código</td>
+              <td>Descripción</td>
+              <td>Precio</td>
+              </tr>
+        </thead>
+        <tbody>
+          {producto.map(producto => (
+            <tr>
+              <td>{producto.codigo}</td>
+              <td>{producto.descripcion}</td>
+              <td>{producto.precio}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  )
+}
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Datos } from './Datos';
+
+ReactDOM.render(
+  <Datos />,
+  document.getElementById('root')
+);
+
+
